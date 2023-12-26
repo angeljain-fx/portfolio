33c669e8841cb4c643c025bc855b60ae50585f8b
Angel Jain <angeljain.fx@gmail.com>
2023-12-27 04:12:29 +0530

    finalising stage 1

--- js/homeCards.js
+++ js/homeCards.js
@@ -1,20 +1,63 @@
 $("#hc2").hide();
 $("#hc3").hide();
+$("#hc4").hide();
 
-$(".num2").click(function () {
+const showCard1 = () => {
+  $("#hc2").fadeOut();
+  $("#hc3").fadeOut();
+  $("#hc4").fadeOut();
+  $("#hc1").fadeIn();
+} 
+const showCard2 = () => {
   $("#hc1").fadeOut();
   $("#hc3").fadeOut();
+  $("#hc4").fadeOut();
   $("#hc2").fadeIn();
+} 
+const showCard3 = () => {
+  $("#hc1").fadeOut();
+  $("#hc2").fadeOut();
+  $("#hc4").fadeOut();
+  $("#hc3").fadeIn();
+} 
+const showCard4 = () => {
+  $("#hc1").fadeOut();
+  $("#hc2").fadeOut();
+  $("#hc3").fadeOut();
+  $("#hc4").fadeIn();
+} 
+
+$(".num1").click(function () {
+  clearInterval(intId);
+  showCard1();
+});
+
+$(".num2").click(function () {
+  clearInterval(intId);
+  showCard2();
 });
 
 $(".num3").click(function () {
-  $("#hc1").fadeOut();
-  $("#hc2").fadeOut();
-  $("#hc3").fadeIn();
+  clearInterval(intId);
+  showCard3();
 });
 
-$(".num1").click(function () {
-  $("#hc2").fadeOut();
-  $("#hc3").fadeOut();
-  $("#hc1").fadeIn();
+$(".num4").click(function () {
+  clearInterval(intId);
+  showCard4();
 });
+
+const arr = [showCard1, showCard2, showCard3, showCard4];
+
+let i = 1;
+const intId = setInterval(() => {
+  if(i === 3) {
+    arr[i]();
+    i = 0;
+  }
+  
+  else {
+    arr[i]();
+    i++;
+  }
+}, 2000);
